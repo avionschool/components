2e4f33062d56704f60d2ad84500ea3158780a9a7
Homer <homer@avionschool.com>
2022-12-01 18:42:26 +0800

    feat: added to Datepicker types

--- src/components/Datepicker/Datepicker.types.ts
+++ src/components/Datepicker/Datepicker.types.ts
@@ -5,7 +5,7 @@ export interface DatepickerProps {
   placeholderText?: string;
   id?: string;
   name: string;
-  onChange?: (field: string, value: any, shouldValidate?: boolean | undefined) => Promise<void> | Promise<FormikErrors<any>>,
+  onChange?: JSONValue,
   value?: Date;
   isError?: boolean;
 };
